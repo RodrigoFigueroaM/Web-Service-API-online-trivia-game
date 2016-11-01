e8c0de1dfb3eb45092dcca96997460d248baeed5
Rodrigo monroy <ruimonroy@csu.fullerton.edu>
2016-11-01 13:33:03 -0700

    fuctnon to handle $.ajaxPost and Get

---
+++ client/httpJSON.js
@@ -0,0 +1,27 @@
+var ajaxPost= function ( url, data, successFunction)
+{
+    $.ajax({
+          type: 'POST',
+              contentType:'application/json',
+              url: url,
+              data: JSON.stringify(data),
+              success: successFunction(),
+              dataType:'json'
+          });
+}
+
+var ajaxGet= function ( url,data,successFunction)
+{
+    $.ajax({
+            type: 'GET',
+            contentType:'application/json',
+            url: url,
+            dataType:'json',
+            success: function(data)
+            {
+                return data;
+            }
+
+          });
+
+};
